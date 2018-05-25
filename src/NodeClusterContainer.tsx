fd7253cb3418e615db025bda380e4c22469a7a69
Eric Chen <ericchen098@gmail.com>
2018-05-24 23:36:15 -0700

    capitalize app.ts

--- src/NodeClusterContainer.tsx
+++ src/NodeClusterContainer.tsx
@@ -39,4 +39,3 @@ export class NodeClusterContainer extends React.Component<NodeClusterContainerPr
     );
   }
 }
-
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import { App } from './app';
-
-// interface HelloProps { compiler: string; framework: string; }
-//
-// // 'HelloProps' describes the shape of props.
-// // State is never set so we use the '{}' type.
-// class Hello extends React.Component<HelloProps, {}> {
-//   render() {
-//     return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
-//   }
-// }
+import { App } from './App';
 
 ReactDOM.render(
   <App/>,
